ab48946c6eea093916937da395443f61b3c8f00d
刘亚军 <liuyajun1999@icloud.com>
2019-07-08 20:50:25 +0800

    Initial commit

---
+++ Example/YJPresentAnimation/YJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJAppDelegate.h
+//  YJPresentAnimation
+//
+//  Created by lyj on 07/08/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/YJPresentAnimation/YJViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJViewController.h
+//  YJPresentAnimation
+//
+//  Created by lyj on 07/08/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJViewController : UIViewController
+
+@end
